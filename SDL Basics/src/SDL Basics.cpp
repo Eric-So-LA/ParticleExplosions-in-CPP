bb3733815c3474d3398860272a855fe7c7db348e
Eric <ericso0303@gmail.com>
2016-08-06 20:58:40 -0700

    [Feature] Implemented SDL third party api

--- SDL Basics/src/SDL Basics.cpp
+++ SDL Basics/src/SDL Basics.cpp
@@ -7,9 +7,18 @@
 //============================================================================
 
 #include <iostream>
+#include <SDL.h>
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	if(SDL_Init(SDL_INIT_VIDEO) < 0){
+		cout << "SDL INIT FAILED" << endl;
+		return 1;
+	}
+
+	cout << "SDL INIT SUCCEEEDEDED" << endl;
+
+	SDL_Quit();
+
 	return 0;
 }
